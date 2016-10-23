fb2887e570bfe204449a5e3aaeb4a2e2ff806e4a
johanngomes <jgomes@thoughtworks.com>
2016-10-23 17:25:56 -0300

    Return a number

--- test/multiples3and5/NumberTest.java
+++ test/multiples3and5/NumberTest.java
@@ -1,5 +1,15 @@
 package multiples3and5;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class NumberTest {
-    
+    @Test
+    public void shouldReturnNumber() {
+        Number number = new Number(5);
+        assertThat(number.value, is(5));
+    }
+
 }
