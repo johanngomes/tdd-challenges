42083202c27ea6555ab0d61c4dd726385f364e2e
johanngomes <jgomes@thoughtworks.com>
2016-10-23 17:25:22 -0300

    Add multiples 3 and 5 challenge

--- src/multiples3and5/Number.java
+++ src/multiples3and5/Number.java
@@ -1,4 +1,10 @@
 package multiples3and5;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Number {
+    public int value;
 }
--- test/multiples3and5/NumberTest.java
+++ test/multiples3and5/NumberTest.java
@@ -1,5 +1,5 @@
 package multiples3and5;
 
 public class NumberTest {
-
+    
 }
