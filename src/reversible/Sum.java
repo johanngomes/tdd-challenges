40562add54c57cacaf63d5e2544fa6c83279a335
johanngomes <jgomes@thoughtworks.com>
2016-11-20 13:24:51 -0300

    [reversible] Get the number os reversible number below one billion

--- src/reversible/Sum.java
+++ src/reversible/Sum.java
@@ -1,7 +1,39 @@
 package reversible;
 
+import reversible.exceptions.NumberNegativeException;
+import reversible.exceptions.NumberNotReversibleException;
+
 public final class Sum {
     public static int sum(int number, int otherNumber) {
         return number + otherNumber;
+    }
+
+    public static int reversibleNumbersBelow(int number) throws NumberNegativeException, NumberNotReversibleException {
+        int reversibleNumbersCount = 0;
+
+        for(int n=10; n <= number; n++) {
+            Number reversedNumber = new Number(n);
+            reversedNumber.reverse();
+
+            boolean isReversible = isReversible(n, reversedNumber);
+
+            reversibleNumbersCount = getReversibleNumbersCount(reversibleNumbersCount, n, reversedNumber, isReversible);
+        }
+
+        return reversibleNumbersCount;
+    }
+
+    private static boolean isReversible(int n, Number numberToBeReverse) throws NumberNegativeException {
+        int sumOfNumberWithReversible = n + numberToBeReverse.getNumber();
+
+        return new Number(sumOfNumberWithReversible).allDigitsAreOdd();
+    }
+
+    private static int getReversibleNumbersCount(int reversibleNumbersCount, int n, Number numberToBeReverse, boolean isReversible) {
+        if( (isReversible) && ((n > 9) && (numberToBeReverse.getNumber() > 9)) ){
+            reversibleNumbersCount++;
+        }
+
+        return reversibleNumbersCount;
     }
 }
--- test/reversible/SumTest.java
+++ test/reversible/SumTest.java
@@ -1,6 +1,8 @@
 package reversible;
 
 import org.junit.Test;
+import reversible.exceptions.NumberNegativeException;
+import reversible.exceptions.NumberNotReversibleException;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -9,5 +11,15 @@ public class SumTest {
     @Test
     public void shouldReturnTheSumOfTwoNumbers() {
         assertThat(Sum.sum(1, 2), is(3));
+    }
+
+    @Test
+    public void shouldReturnTheNumberOfReversibleNumbersBelow1000() throws NumberNotReversibleException, NumberNegativeException {
+        assertThat(Sum.reversibleNumbersBelow(1000), is(120));
+    }
+
+    @Test
+    public void shouldReturnTheNumberOfReversibleNumbersBelow10000000() throws NumberNotReversibleException, NumberNegativeException {
+        assertThat(Sum.reversibleNumbersBelow(10000000), is(73370));
     }
 }
